87c8100f2acb327ee75b2616105d1d1fe0a4d11b
alper <alper@MSI>
2023-03-04 23:36:36 +0300

    Proje dosyası ekle.

---
+++ DataAccessLayer/Repositories/BlogRepository.cs
@@ -0,0 +1,48 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class BlogRepository : IBlogDal
+    {
+        public void AddBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Blogs.Add(blog);
+            context.SaveChanges();
+        }
+
+        public void DeleteBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Blogs.Remove(blog);
+            context.SaveChanges();
+        }
+
+        public Blog GetById(int id)
+        {
+            using var context = new Context();
+            return context.Blogs.Find(id);
+        }
+
+        public List<Blog> ListAllBlog()
+        {
+            using var context = new Context();
+            return context.Blogs.ToList();
+        }
+
+        public void UpdateBlog(Blog blog)
+        {
+            using var context = new Context();
+            context.Blogs.Update(blog);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ DataAccessLayer/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+        public void Delete(T item)
+        {
+            using var context = new Context();
+            context.Remove(item);
+            context.SaveChanges();
+        }
+
+        public T GetById(int id)
+        {
+            using var context = new Context();
+            return context.Set<T>().Find(id);
+        }
+
+        public List<T> GetListAll()
+        {
+            using var context = new Context();
+            return context.Set<T>().ToList();
+        }
+
+        public void Insert(T item)
+        {
+            using var context = new Context();
+            context.Add(item);
+            context.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            using var context = new Context();
+            context.Update(item);
+            context.SaveChanges();
+        }
+    }
+}
